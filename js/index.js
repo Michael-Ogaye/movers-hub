485d25ea8f97848b7de1232deac4cbf190c99b88
Charlotte Natasha <natashaonyango97@gmail.com>
2022-03-23 22:21:25 +0100

    used toggle javascript

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
-/*
-const inputs = document.querySelectorAll('.field');
-const toggle_btn = document.querySelectorAll('.toggle');
-const section = document.querySelectorAll('section')
+const inputs = document.querySelectorAll(".input");
+const toggle_btn = document.querySelectorAll(".toggle");
+const section = document.querySelector("section");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
 
-inputs.forEach(inp => {
-    inp.addEventListener('focus', () =>{
-        inp.classList.add('active')
-    });
-    inp.addEventListener('blur', () => {
-        if (inp.value != '') return;
-        inp.classList.remove('active');
-    });
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
+  });
 });
 
-toggle_btn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        section.classList.toggle('.logIn');
-    });
+toggle_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    section.classList.toggle("logIn");
+  });
 });
-*/
 
 
 
@@ -39,13 +39,6 @@ toggle_btn.forEach(btn => {
 
 
 
-
-
-$(document).ready(function (){
-    $('.toggle').click(function(){
-        $('#move').show();
-    })
-})
 
 
 
