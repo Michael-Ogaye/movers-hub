9da48ff4985d35e51f8808ba5f5e3643218af937
Michael Ogaye <hogaeymic@gmail.com>
2022-03-23 21:43:18 +0300

    ft: incoporated the search feature

--- js/profile.js
+++ js/profile.js
@@ -6,6 +6,33 @@ booKBtns.click(function(){
 })
 
 
+//searching for names
+
+$('input[type=search]')[0].addEventListener('keyup',()=>{
+
+    let searchFieldVal=$('input[type=search]').val().toLowerCase();
+    let profiles=$('section#profiles')
+    profiles.each(function(profile){
+        
+        let searchArea= $(this).find('h5').text().toLowerCase();
+    if(searchArea.indexOf(searchFieldVal)!=-1){
+        $(this).show();
+
+    
+    }
+    else{
+        $(this).hide();
+    }
+
+
+    })
+
+})
+
+
+
+
+
 
 
 
