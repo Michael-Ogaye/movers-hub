308cd3eb5f6213ab42cf3d728c708d68018bf1d7
naomimwangi <naomimwangi7@gmail.com>
2022-03-23 21:46:13 +0300

    add style to cards

--- js/contact.js
+++ js/contact.js
@@ -1,22 +1,14 @@
-$document.ready (function (){
-    $(".row").items({
-        loop: true,
-        infinite: true,
-        autoplay: true,
-        speed: 800,
-        autoplaySpeed: 2000,
-        slideToShow: 2,
-        slideToScroll: 2,
 
-    });
-    $("#contact").submit(function () {
-        event.preventDefault();
-        let name = $("input#text").val();
-        let email = $("input#email").val();
-        let message = $("textarea#message").val();
-        console.log(contact);
-        alert(name + " " + "we have received your message. Thank you for being here")
-    });
+$("#contact").submit(function (event) {
+    event.preventDefault()
+    let name = $("input#text").val();
+    let email = $("input#email").val();
+    let message = $("textarea#message").val();
+    alert(name + " " + "we have received your message. Thank you for being here.");
+});
+
+$(".card").mouseover(function () {
+    $(".comment").show();
 });
 
 
