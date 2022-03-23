00dc8bf282a1d1cb2b346f3017a67dc90f3d9157
esthercate <vuthi.esther@gmail.com>
2022-03-23 16:10:47 +0300

    formats js file

--- js/booking.js
+++ js/booking.js
@@ -1,5 +1,13 @@
 // Business Logic
-function ContactMover(name, number, movingFrom, movingTo, date, houseSize, message) {
+function ContactMover(
+  name,
+  number,
+  movingFrom,
+  movingTo,
+  date,
+  houseSize,
+  message
+) {
   this.name = name;
   this.number = number;
   this.movingFrom = movingFrom;
@@ -10,13 +18,23 @@ function ContactMover(name, number, movingFrom, movingTo, date, houseSize, messa
 };
 
 ContactMover.prototype.trackBooking = function () {
-  return this.name + this.number + this.movingFrom + this.movingTo + this.date + this.houseSize + this.message;
+  return (
+    this.name +
+    this.number +
+    this.movingFrom +
+    this.movingTo +
+    this.date +
+    this.houseSize +
+    this.message
+  );
 };
 
 //User Interface Logic
 $(document).ready(function () {
-  $("form#contactForm").submit(function (event) {
+  $("#submitBtn").click(function (event) {
     event.preventDefault();
+
+    //collecting user inputs
     var clientName = $("#name").val();
     var pNumber = $("#tel").val();
     var currentLocation = $("#moving-from").val();
@@ -25,16 +43,27 @@ $(document).ready(function () {
     var numberOfRooms = $("#rooms").val();
     var additionalInfo = $("#message").val();
 
+    //Creates an object instance
+    var bookMover = new ContactMover(
+      clientName,
+      pNumber,
+      currentLocation,
+      destination,
+      movingDate,
+      numberOfRooms,
+      additionalInfo
+    );
+
+    console.log(bookMover.trackBooking());
+
   });
 
   // Disable previous dates
   var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0');
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0");
   var yyyy = today.getFullYear();
 
-  today = yyyy + '-' + mm + '-' + dd;
-  $('#date-picker').attr('min', today);
-
-
+  today = yyyy + "-" + mm + "-" + dd;
+  $("#date-picker").attr("min", today);
 });
