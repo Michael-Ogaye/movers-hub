191df50dfa82f5b7dfe0b90caa591ae71ba03154
Michael Ogaye <hogaeymic@gmail.com>
2022-03-24 09:19:21 +0300

    ft:navbar imaggggge resize

--- js/profile.js
+++ js/profile.js
@@ -13,7 +13,7 @@ $('input[type=search]')[0].addEventListener('keyup',()=>{
     let searchFieldVal=$('input[type=search]').val().toLowerCase();
     let profiles=$('section#profiles')
     profiles.each(function(profile){
-        
+
         let searchArea= $(this).find('h5').text().toLowerCase();
     if(searchArea.indexOf(searchFieldVal)!=-1){
         $(this).show();
@@ -28,6 +28,43 @@ $('input[type=search]')[0].addEventListener('keyup',()=>{
     })
 
 })
+
+
+//creating the profiles object
+
+
+let allServiceProviders=[];
+
+function Provider (name,location,rates,contact,capacity){
+    this.name=name
+    this.location=location
+    this.rates=rates
+    this.contact=contact
+    this.capacity=capacity
+
+}
+
+// Data from existing users
+function users (){
+    $('section#profiles').each(function(){
+        let namep=$(this).find('h5').text()
+        let loc=$(this).find('.loc p').text()
+        let rates=$(this).find('.rate p').text()
+        let contacts=$(this).find('.qul ul li p small').text()
+        let capacity=$(this).find('.det p').text()
+    
+        let aProvider= new Provider(namep,loc,rates,contacts,capacity);
+    
+        allServiceProviders.push(aProvider);
+        return allServiceProviders;
+        
+    })
+    
+}
+
+users()
+console.log(allServiceProviders[3])
+
 
 
 
