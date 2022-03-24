ecfb3d9659c42ac32f20d9e34c85829734a06c77
esthercate <vuthi.esther@gmail.com>
2022-03-25 01:38:23 +0300

    changes

--- js/profile.js
+++ js/profile.js
@@ -1,69 +1,67 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-// let booKBtns=$('#profiles button');
-// booKBtns.click(function(){
-//     window.location.href=''
-// })
+    // let booKBtns=$('#profiles button');
+    // booKBtns.click(function(){
+    //     window.location.href=''
+    // })
 
 
-//searching for names
+    //searching for names
 
-$('input[type=search]')[0].addEventListener('keyup',()=>{
+    $('input[type=search]')[0].addEventListener('keyup', () => {
 
-    let searchFieldVal=$('input[type=search]').val().toLowerCase();
-    let profiles=$('section#profiles')
-    profiles.each(function(profile){
+        let searchFieldVal = $('input[type=search]').val().toLowerCase();
+        let profiles = $('section#profiles')
+        profiles.each(function (profile) {
 
-        let searchArea= $(this).find('h5').text().toLowerCase();
-    if(searchArea.indexOf(searchFieldVal)!=-1){
-        $(this).show();
+            let searchArea = $(this).find('h5').text().toLowerCase();
+            if (searchArea.indexOf(searchFieldVal) != -1) {
+                $(this).show();
 
-    
-    }
-    else{
-        $(this).hide();
-    }
 
+            } else {
+                $(this).hide();
+            }
+
+
+        })
 
     })
 
-})
+
+    //creating the profiles object
 
 
-//creating the profiles object
+    let allServiceProviders = [];
 
+    function Provider(name, location, rates, contact, capacity) {
+        this.name = name
+        this.location = location
+        this.rates = rates
+        this.contact = contact
+        this.capacity = capacity
+    }
 
-let allServiceProviders=[];
+    // Data from existing users
+    function users() {
+        $('section#profiles').each(function () {
+            let namep = $(this).find('h5').text()
+            let loc = $(this).find('.loc p').text()
+            let rates = $(this).find('.rate p').text()
+            let contacts = $(this).find('.qul ul li p small').text()
+            let capacity = $(this).find('.det p').text()
 
-function Provider (name,location,rates,contact,capacity){
-    this.name=name
-    this.location=location
-    this.rates=rates
-    this.contact=contact
-    this.capacity=capacity
+            let aProvider = new Provider(namep, loc, rates, contacts, capacity);
 
-}
+            allServiceProviders.push(aProvider);
+            return allServiceProviders;
 
-// Data from existing users
-function users (){
-    $('section#profiles').each(function(){
-        let namep=$(this).find('h5').text()
-        let loc=$(this).find('.loc p').text()
-        let rates=$(this).find('.rate p').text()
-        let contacts=$(this).find('.qul ul li p small').text()
-        let capacity=$(this).find('.det p').text()
-    
-        let aProvider= new Provider(namep,loc,rates,contacts,capacity);
-    
-        allServiceProviders.push(aProvider);
-        return allServiceProviders;
-        
-    })
-    
-}
+        })
 
-users()
-console.log(allServiceProviders[3])
+    }
+
+    users()
+    console.log(allServiceProviders[3])
 
 
 
